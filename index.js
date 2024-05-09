27649d3f4888acd9a5f3c4e6f0321c09b8af34fa
Vanlsh <iparan800@ukr.net>
2024-05-09 16:15:58 +0300

    feat(feature): add event listeners

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const form = document.querySelector("#form");
+const inputLable = document.querySelector("#form label");
+
+const sortByNameBtn = document.querySelector("#sortByName");
+const sortByValueBtn = document.querySelector("#sortByValue");
+const deleteBtn = document.querySelector("#delete");
+const parseBtn = document.querySelector("#parse");
+
+const showDataInput = document.querySelector("#data");
+
+// save user data as array of object {key, value}
+let userData = [];
+// to check how to display user data
+let isXml = false;
+
+form.addEventListener("click", onSubmit);
+sortByNameBtn.addEventListener("click", onSortByName);
+sortByValueBtn.addEventListener("click", onSortByValue);
+deleteBtn.addEventListener("click", onDelete);
+parseBtn.addEventListener("input", onchange);
+
+function onSubmit(e) {
+  e.preventDefault();
+  const { value } = e.currentTarget.elements.data;
+}
+function onSortByName() {}
+function onSortByValue() {}
+function onDelete() {}
+function onChangeFormat(e) {}
