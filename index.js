b24920f053cb3da7bbca797902d0be5168760fa5
Vanlsh <iparan800@ukr.net>
2024-05-13 15:50:12 +0300

    feat

--- index.js
+++ index.js
@@ -17,7 +17,9 @@ sortByNameBtn.addEventListener("click", onSortByName);
 sortByValueBtn.addEventListener("click", onSortByValue);
 deleteBtn.addEventListener("click", onDelete);
 parseBtn.addEventListener("click", onChangeFormat);
-
+showDataInput.addEventListener("input", () => {
+  return;
+});
 // EVENTS
 function onSubmit(e) {
   e.preventDefault();
@@ -45,8 +47,63 @@ function onSortByValue() {
 }
 
 function onDelete() {
-  setPairs([]);
-  showDataInput.value = "";
+  console.log(showDataInput.selectionStart);
+  console.log(showDataInput.selectionEnd);
+  console.log(showDataInput.value.length);
+  const { value, selectionStart, selectionEnd } = showDataInput;
+  if (selectionStart == selectionEnd) return;
+
+  const range = getLengthOfStrings({ value, selectionStart, selectionEnd });
+
+  if (!range) return;
+  const { startIndex, endIndex } = range;
+  console.log("startIndex", startIndex);
+  console.log("endIndex", endIndex);
+
+  const pairs = getPairs();
+  pairs.splice(startIndex, endIndex - startIndex + 1);
+  console.log(pairs);
+  setPairs(pairs);
+  renderList(pairs);
+}
+
+function getLengthOfStrings({ value, selectionStart, selectionEnd }) {
+  console.log({ value: value.trim() });
+  const pairLengthes = value
+    .trim()
+    .split("\n")
+    .map((pair) => pair.length + 1);
+
+  const isXml = getIsXml();
+
+  let pairEndIndexes = [];
+  let startIndex = 0;
+  let endIndex = 0;
+
+  if (isXml) {
+    let counter = pairLengthes[0];
+    console.log(counter + " selectionEnd -> " + selectionEnd);
+    console.log(counter >= selectionEnd);
+    if (counter >= selectionEnd) return null;
+
+    for (let i = 1; i < pairLengthes.length - 1; i++) {
+      counter += pairLengthes[i];
+      pairEndIndexes.push(counter);
+    }
+    console.log(pairEndIndexes.at(-1) + "" + selectionStart);
+    if (pairEndIndexes.at(-1) <= selectionStart) return null;
+    const lastItem = pairEndIndexes[pairEndIndexes.length - 1] + pairLengthes.at(-1) - 1;
+
+    pairEndIndexes[pairEndIndexes.length - 1] = lastItem;
+  } else {
+    pairEndIndexes = pairLengthes.reduce((acc, nextPair) => [...acc, acc.at(-1) + nextPair || nextPair], []);
+  }
+  // console.log(pairEndIndexes);
+  startIndex = pairEndIndexes.findIndex((item) => item > selectionStart);
+  endIndex = pairEndIndexes.findIndex((item) => item >= selectionEnd);
+  // console.log(startIndex);
+  // console.log(endIndex);
+  return { startIndex, endIndex };
 }
 
 function onChangeFormat(e) {
@@ -74,6 +131,10 @@ function parsePair(text) {
 }
 
 function renderList(pairs) {
+  if (!pairs.length) {
+    showDataInput.value = "";
+    return;
+  }
   const isXml = parseBtn.checked;
   if (isXml) {
     showDataInput.value = makeXml(pairs);
@@ -83,10 +144,7 @@ function renderList(pairs) {
 }
 
 function makeXml(pairs) {
-  const text = pairs.reduce(
-    (acc, { key, value }) => (acc += `  <${key}>${value}</${key}>\n`),
-    ""
-  );
+  const text = pairs.reduce((acc, { key, value }) => (acc += `  <${key}>${value}</${key}>\n`), "");
   return "<data>\n" + text + "</data>";
 }
 
@@ -95,13 +153,19 @@ function makeSimplePairs(pairs) {
 }
 
 function sortPairs(pairs, type) {
-  return pairs.toSorted((firstPair, secondPair) =>
-    firstPair[type].localeCompare(secondPair[type])
-  );
+  const sortedPairs = pairs.toSorted((firstPair, secondPair) => firstPair[type].localeCompare(secondPair[type]));
+  setPairs(sortedPairs);
+  return sortedPairs;
 }
 
 // Local Storage
-
+setPairs([
+  { key: "T1111", value: "2222" },
+  { key: "323232", value: "1111111" },
+  { key: "7777", value: "56565" },
+  { key: "2324", value: "5555" },
+  { key: "43534", value: "546546" },
+]);
 function getPairs() {
   return getFromLS(DATA_KEY_LS) || [];
 }
